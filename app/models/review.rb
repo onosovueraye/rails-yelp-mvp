0d670cd833e3e2698a59f05b78bc8235733ae4cc
Onos Ovueraye <onosovueraye@gmail.com>
2023-05-24 14:04:56 +0100

    completed the yelp mvp task

---
+++ app/models/review.rb
@@ -0,0 +1,8 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :rating, presence: true
+  validates :rating, inclusion: { in: (0..5) }
+  validates :rating, numericality: { only_integer: true }
+  validates :content, presence: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "faker"
+
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Book.title,
+    address: Faker::Address.street_name,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save!
+end
+puts 'Finished seeding'
+
+Restaurant.create(name: "Guy's", address: "shoreditch", phone_number: "123456789", category: "french")
